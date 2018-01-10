3c66cd9e0ad1d83d3052a90275aefebded7c9b35
Dov Grobgeld <dov.grobgeld@gmail.com>
2018-01-10 14:02:49 +0200

    Some windows fixes (still not perfect).
    - Hardcoded path to the packages directory.
    - Fixed font color problem on windows.

--- emacs/init-helm.el
+++ emacs/init-helm.el
@@ -1,8 +1,18 @@
+(setq load-path (append
+                 (list
+                  (concat emacs-git "/packages/helm-20171230.854")
+                  (concat emacs-git "/packages/helm-core-20171230.43/")
+                  (concat emacs-git "/packages/async-20171015.2239/")
+                  emacs-git
+                  )
+                 load-path))
+		  
 (require 'helm-config)
 
 ;; override standard commands with their corresponding helm commands
 
 ; execute-extended-command → helm-M-x
+;(global-set-key "\M-x" 'execute-extended-command)
 (global-set-key "\M-x" 'helm-M-x)
 
 ; switch-to-buffer → helm-buffers-list
