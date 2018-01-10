efebcdcb4acd51d029df4d5281ba2e4ed45d6981
Dov Grobgeld <dov.grobgeld@xjet3d.com>
2018-01-10 13:48:28 +0200

    Changed key-bindings to make helm default for some ops.

--- emacs/init-helm.el
+++ emacs/init-helm.el
@@ -1,3 +1,14 @@
 (require 'helm-config)
 
+;; override standard commands with their corresponding helm commands
+
+; execute-extended-command → helm-M-x
+(global-set-key "\M-x" 'helm-M-x)
+
+; switch-to-buffer → helm-buffers-list
+(global-set-key "\C-xb" 'helm-buffers-list)
+
+
+(setq helm-buffer-max-length 30)
+
 (provide 'init-helm)
