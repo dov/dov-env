1110fee3970a9f2ab6eb5e4dc9ad086cd7784156
Dov Grobgeld <dov.grobgeld@xjet3d.com>
2021-07-12 11:16:49 +0300

    Added emacs with-editor

---
+++ emacs/init-with-editor.el
@@ -0,0 +1,7 @@
+(setq load-path (append
+                 (list
+                  (concat emacs-git "/packages/with-editor-20210524.1654/"))
+                 load-path))
+(load "with-editor.el")
+
+(provide 'init-with-editor)
