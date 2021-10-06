e910d7bc178fe91969335ea12a6702d76786df0b
Dov Grobgeld <dov.grobgeld@gmail.com>
2021-10-06 21:37:04 +0300

    Updated magit to version v3.3.0

--- emacs/magit/git-commit-pkg.el
+++ emacs/magit/git-commit-pkg.el
@@ -1,9 +1,7 @@
-(define-package "magit" "3.3.0"
-  "A Git porcelain inside Emacs."
+(define-package "git-commit" "3.3.0"
+  "Edit Git commit messages."
   '((emacs "25.1")
     (dash "2.19.1")
-    (git-commit "3.3.0")
-    (magit-section "3.3.0")
     (transient "0.3.6")
     (with-editor "3.0.5"))
   :homepage "https://magit.vc"
